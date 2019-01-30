a1b75556bf24b853e0b61454473d410f7c25d996
Roman Furman <roman.furman@uptech.team>
2019-01-30 21:35:00 +0200

    Initial Commit

---
+++ SlackStatus/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SlackStatus
+//
+//  Created by Roman on 1/30/19.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SlackStatus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SlackStatus
+//
+//  Created by Roman on 1/30/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
